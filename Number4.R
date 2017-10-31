843469c455c310ab7c226a911d01e75b55834930
tay bock <taylor.bock@stlcop.edu>
2017-10-31 14:49:07 -0500

    4
    added male data

--- Number4.R
+++ Number4.R
@@ -12,3 +12,8 @@ ggplot(data = dat, aes(x = F_mass)) + labs(x = "Female Mass g") +
 #scaled to log
 ggplot(data = dat, aes(x = F_mass)) + labs(x = "Female Mass g") +
   geom_histogram() +  scale_x_log10()
+
+#add the males
+ggplot(data = dat, aes(x = F_mass)) + labs(x = "Female Mass g") +
+  geom_histogram(fill = "cyan", alpha = 0.3) +  geom_histogram(aes(x = M_mass), alpha = 0.3) + 
+  scale_x_log10()
