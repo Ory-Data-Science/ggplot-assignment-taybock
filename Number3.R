4ba068b99a6135f888290f3eda23c1dc8ad28f8c
tay bock <taylor.bock@stlcop.edu>
2017-10-31 14:07:13 -0500

    problem 3
    part 1

--- Number3.R
+++ Number3.R
@@ -5,4 +5,6 @@ library(tidyverse)
 dat <- read.csv(url("http://esapubs.org/archive/ecol/E084/093/Mammal_lifehistories_v2.txt"), 
                 sep = "\t", nrows = 1440, na.strings = c("-999.00", "999.00"))
 
-ggplot(data = dat) + geom_point(mapping = aes(x = "mass(g)", y = "newborn(g)"))
+ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
+  labs(x = "Adult Mass", y = "Newborn Mass") +
+  geom_point()
